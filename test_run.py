383e46ca6b14d3e28ba1e035305c7113a6c6de64
Alireza Chamanzar <alirezach70@gmail.com>
2025-08-21 01:57:53 +0330

    remove unnecessary files

--- test_run.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env python3
-# -*- coding: utf-8 -*-
-
-from update_price import USD2RialsUpdater
-
-if __name__ == '__main__':
-    updater = USD2RialsUpdater()
-    ok = updater.run()
-    print('Result:', ok)
